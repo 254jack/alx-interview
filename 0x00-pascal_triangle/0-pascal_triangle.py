49e6897f85cb94d79417d5925f9fe7f9a76342ec
254jack <taixonj@gmail.com>
2024-03-11 06:16:35 +0300

    first commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to n rows.
+    Returns:
+        List[List[int]]: Pascal's triangle as a list of lists.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
