a0edcbf9bac48ef055befd398fdee4f97f18e863
254jack <taixonj@gmail.com>
2024-03-15 00:18:38 +0300

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
- Unlocked the boxes
+ Unlocked boxes
 """
 
 
 def canUnlockAll(boxes):
     """
-        Look if the box is unlocked or not
+        Look when the box is unlocked
         Return true if is unlocked or false
     """
 
-    # Look to the unlocked boxes
     unlocked_box = [0]
     for i in unlocked_box:
         for j in boxes[i]:
@@ -18,7 +17,6 @@ def canUnlockAll(boxes):
                 if j not in unlocked_box:
                     unlocked_box.append(j)
 
-    # Look the the length of the open box and all the boxes
     if len(boxes) == len(unlocked_box):
         return True
     else:
