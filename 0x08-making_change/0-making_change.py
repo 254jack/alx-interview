a48642623f404c27e0749ae8cc0ecd49d2953732
254jack <taixonj@gmail.com>
2024-05-03 20:17:04 +0300

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+coin problem solver module
+'''
+
+
+def makeChange(coins, total):
+    '''
+    A method which returns fewest number of coins needed to
+    meet a given total coin number if total is not less than 0
+    '''
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
+    
